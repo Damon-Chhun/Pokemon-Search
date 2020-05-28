6ea10fc631431c7b2aaa4072a712f3b04a607493
Damon Chhun <damonchhun7@gmail.com>
2020-05-28 12:47:04 -0700

    styling changes

--- pokemon/src/CardArray.js
+++ pokemon/src/CardArray.js
@@ -1,16 +1,26 @@
 import React from "react";
 import Card from "./Card";
+import styled from "styled-components";
 
 const cardArray = ({ pokemons }) => {
   return (
-    <div>
+    <PokemonGrid>
       {pokemons.map((pokemons, index) => {
         return (
           <Card name={pokemons.name} key={pokemons.name} index={index + 1} />
         );
       })}
-    </div>
+    </PokemonGrid>
   );
 };
 
 export default cardArray;
+
+const PokemonGrid = styled.div`
+  border: solid red;
+  display: grid;
+  grid-template-columns: repeat(5, 15%);
+  grid-column-gap: 5%;
+  grid-row-gap: 10px;
+  padding: 10px;
+`;
