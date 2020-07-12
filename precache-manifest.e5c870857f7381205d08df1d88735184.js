c26341c76ed8cc69ed38ac3f1484a84abc15761d
Damon Chhun <damonchhun7@gmail.com>
2020-07-12 10:02:04 -0700

    Updates

---
+++ precache-manifest.e5c870857f7381205d08df1d88735184.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3eed9a24f95b4777a0f582edfafa757",
+    "url": "/Pokemon-Search/index.html"
+  },
+  {
+    "revision": "2831170309fb6da5b1d5",
+    "url": "/Pokemon-Search/static/css/main.592e22b6.chunk.css"
+  },
+  {
+    "revision": "de59fe2681d8c0254663",
+    "url": "/Pokemon-Search/static/js/2.7fe8850a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Pokemon-Search/static/js/2.7fe8850a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2831170309fb6da5b1d5",
+    "url": "/Pokemon-Search/static/js/main.f7a4e8e7.chunk.js"
+  },
+  {
+    "revision": "8fa7525074ff3465f237",
+    "url": "/Pokemon-Search/static/js/runtime-main.c2b29710.js"
+  },
+  {
+    "revision": "083ed65006cc9ac8adc19da0de8ca6ec",
+    "url": "/Pokemon-Search/static/media/logo.083ed650.png"
+  }
+]);
