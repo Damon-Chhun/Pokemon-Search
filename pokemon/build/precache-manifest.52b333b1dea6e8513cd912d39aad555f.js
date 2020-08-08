24e83726f02b1ebb04bb0d67c0501a65913f99a2
Damon Chhun <damonchhun7@gmail.com>
2020-08-07 18:14:13 -0700

    static

--- pokemon/build/precache-manifest.52b333b1dea6e8513cd912d39aad555f.js
+++ pokemon/build/precache-manifest.52b333b1dea6e8513cd912d39aad555f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1edeb67050d84e517a683bb89fdbae64",
+    "revision": "0b38530ce8e1d2f46de407b682789e7f",
     "url": "/Pokemon-Search/index.html"
   },
   {
-    "revision": "ac7222c5597dcebf1e74",
+    "revision": "7ab6e638b638c610ab6e",
     "url": "/Pokemon-Search/static/css/main.883f4706.chunk.css"
   },
   {
-    "revision": "4e3ebdd4e9d15639e408",
-    "url": "/Pokemon-Search/static/js/2.e86c67f0.chunk.js"
+    "revision": "a1a4019b03437534ab58",
+    "url": "/Pokemon-Search/static/js/2.97e32ad9.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/Pokemon-Search/static/js/2.e86c67f0.chunk.js.LICENSE.txt"
+    "url": "/Pokemon-Search/static/js/2.97e32ad9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ac7222c5597dcebf1e74",
-    "url": "/Pokemon-Search/static/js/main.cc4604e1.chunk.js"
+    "revision": "7ab6e638b638c610ab6e",
+    "url": "/Pokemon-Search/static/js/main.82b14f2f.chunk.js"
   },
   {
     "revision": "8fa7525074ff3465f237",
--- pokemon/build/service-worker.js
+++ pokemon/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Pokemon-Search/precache-manifest.94f6b8a251752b7f8dc916e643b2abff.js"
+  "/Pokemon-Search/precache-manifest.52b333b1dea6e8513cd912d39aad555f.js"
 );
 
 self.addEventListener('message', (event) => {
